dcfa4227012f44b62eac27655c0c6b91c976ae22
Dody Agung Saputro <dodyags@gmail.com>
2021-01-24 12:37:11 +0700

    fix botman controller is not found

--- routes/botman.php
+++ routes/botman.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\BotManController;
+
 $botman = resolve("botman");
 
 $botman->hears("Hi", function ($bot) {
     $bot->reply("Hello!");
 });
 
-$botman->hears("Start conversation", "BotManController@startConversation");
+$botman->hears(
+    "Start conversation",
+    BotManController::class . "@startConversation"
+);
+
+$botman->fallback(BotManController::class . "@fallback");
