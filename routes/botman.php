47445f95428d1123ca52a917dbaf24217217c3d8
Dody <dodyags@gmail.com>
2024-02-15 09:50:24 +0700

    BREAKING CHANGE: change PHP Laravel to Node NestJS

--- routes/botman.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\BotManController;
-
-$botman = resolve("botman");
-
-$botman->hears(env("BOT_COMMAND_START"), BotManController::class . "@start");
-
-$botman->fallback(BotManController::class . "@fallback");
