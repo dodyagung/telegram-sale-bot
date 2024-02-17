ae70250b0068708757a3a6f3bccc150a0dca7c42
Dody <dodyags@gmail.com>
2024-02-18 01:35:44 +0700

    update welcome message

--- src/sale/sale.update.ts
+++ src/sale/sale.update.ts
@@ -1,25 +1,41 @@
-import { Hears, Start, Update, Ctx, On, Help, Sender } from 'nestjs-telegraf';
-import { Context } from 'telegraf';
+import { Hears, Start, Update, Ctx, Sender } from 'nestjs-telegraf';
+import { Context, Markup } from 'telegraf';
+import { format } from 'date-fns';
+import { id } from 'date-fns/locale';
 
 @Update()
 export class SaleUpdate {
   @Start()
-  async start(@Ctx() ctx: Context) {
-    await ctx.reply('Welcome');
-  }
+  @Hears(['hi', 'hello', 'hey', 'qq', 'a'])
+  async onGreetings(
+    @Ctx() ctx: Context,
+    @Sender('first_name') firstName: string,
+    @Sender('last_name') lastName: string,
+  ): Promise<void> {
+    const today = format(new Date(), "EEEE, dd MMMM yyyy '\\-' HH:mm 'WIB'", {
+      locale: id,
+    });
 
-  @Help()
-  async help(@Ctx() ctx: Context) {
-    await ctx.reply('Send me a sticker');
-  }
+    const keyboard = Markup.inlineKeyboard([
+      [
+        Markup.button.callback('💰 My Sale', 'sale'),
+        Markup.button.callback('👤 My Profile', 'profile'),
+      ],
+      [
+        Markup.button.callback('❓ Tutorial', 'tutorial'),
+        Markup.button.callback('🤖 About', 'about'),
+      ],
+    ]);
 
-  @On('sticker')
-  async on(@Ctx() ctx: Context) {
-    await ctx.reply('👍');
-  }
+    const message = `
+*🏠 Welcome*
+      
+Hello *${firstName}${lastName ? ' ' + lastName : ''}*, I'm [telegram\\-sale\\-bot](https://github.com/dodyagung/telegram-sale-bot)\\. Now is *${today}*, what can I help you today?
+    `;
 
-  @Hears(['hi', 'hello', 'hey', 'qq'])
-  async onGreetings(@Sender('first_name') firstName: string): Promise<string> {
-    return `Hey ${firstName}`;
+    await ctx.replyWithMarkdownV2(message, {
+      disable_web_page_preview: true,
+      reply_markup: keyboard.reply_markup,
+    });
   }
 }
