3b5dff7b79525def36c01c53761283cff0cb0812
Dody <dodyags@gmail.com>
2024-03-12 08:05:45 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { getBotToken } from 'nestjs-telegraf';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const bot = app.get(getBotToken('sale'));
-  app.use(bot.webhookCallback('/'));
-  await app.listen(3000);
+  await NestFactory.createApplicationContext(AppModule);
 }
 bootstrap();
