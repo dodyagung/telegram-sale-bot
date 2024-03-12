b464bae9cb08bd930f852515962f9f89875078df
Dody <dodyags@gmail.com>
2024-03-12 10:12:10 +0700

    revert to webhook

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,30 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TelegrafModule, TelegrafModuleOptions } from 'nestjs-telegraf';
 import { SaleModule } from './sale/sale.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     TelegrafModule.forRootAsync({
+      botName: 'sale',
       imports: [ConfigModule],
-      inject: [ConfigService],
       useFactory: async (
         configService: ConfigService,
       ): Promise<TelegrafModuleOptions> => ({
         token: configService.get<string>('TELEGRAM_SALE_BOT_TOKEN')!,
+        include: [SaleModule],
+        launchOptions: {
+          webhook: {
+            domain: configService.get<string>('TELEGRAM_SALE_BOT_WEBHOOK')!,
+            hookPath: '/',
+          },
+        },
       }),
+      inject: [ConfigService],
     }),
     SaleModule,
     ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
     PrismaModule,
   ],
 })
--- src/sale/sale.service.ts
+++ src/sale/sale.service.ts
@@ -1,27 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Logger, Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { posts } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SaleService {
+  private readonly logger = new Logger(SaleService.name);
+
   constructor(private prismaService: PrismaService) {}
 
-  @Cron('0 */4 * * * *') // every 4 minutes
-  async ping() {
-    this.prismaService.$queryRaw`select 1`;
-  }
+  // @Cron('0 */4 * * * *') // every 4 minutes
+  // async ping() {
+  //   const ping = await this.prismaService.$queryRaw`select 1`;
+  //   this.logger.log(`Database ping: ${JSON.stringify(ping)}`);
+  // }
 
-  @Cron('0 * * * * *') // every minute
-  async pingTest() {
-      this.prismaService.$queryRaw`select 1`;
-      console.log('pingTest every minute')
-  }
-
-  async mySale() {
-    const a: posts | null = await this.prismaService.posts.findFirst({
-      where: { id: Number(711) },
-    });
-    console.log(a?.post);
-  }
+  // async mySale() {
+  //   const a: posts | null = await this.prismaService.posts.findFirst({
+  //     where: { id: Number(711) },
+  //   });
+  //   console.log(a?.post);
+  // }
 }
