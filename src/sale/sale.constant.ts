c48903d5656bc1f0ddc0a7ad0a6944b84277d01f
Dody <dodyags@gmail.com>
2024-05-06 09:56:42 +0700

    fix: nomenclature NO_GROUP_MESSAGE

--- src/sale/sale.constant.ts
+++ src/sale/sale.constant.ts
@@ -92,4 +92,4 @@ export const NOW = CONF_NOW;
 export const FALLBACK_MESSAGE =
   '**Wrong input, invalid command or something error.**\n\nPlease restart by clicking /start.';
 
-export const NO_GROUP_MESSAGE = `**You can't send me a "start" command in a group.**\n\nPlease send it via [my private chat](tg://user?id=${process.env.TELEGRAM_SALE_BOT_TOKEN!.split(':')[0]}).`;
+export const NO_GROUP_MESSAGE = `**You can't send me a "start" command from inside a group.**\n\nPlease send it via [my private chat](tg://user?id=${process.env.TELEGRAM_SALE_BOT_TOKEN!.split(':')[0]}).`;
